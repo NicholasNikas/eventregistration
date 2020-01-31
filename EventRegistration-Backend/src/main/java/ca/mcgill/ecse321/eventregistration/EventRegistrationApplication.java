f930bc817d049e5ccb40fa41419eef48f847bf56
Nicholas Nikas <nicholas.nikas@outlook.com>
2020-01-31 16:02:41 -0500

    fixing build -xtest issues

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
@@ -1,22 +1,9 @@
 package ca.mcgill.ecse321.eventregistration;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@SpringBootApplication
-public class EventRegistrationApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EventRegistrationApplication.class, args);
-	}
-
-}
-
-package ca.mcgill.ecse321.eventregistration;
 
 @RestController
 @SpringBootApplication
